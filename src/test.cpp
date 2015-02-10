ab0dedd8bc5584010cbe969189659a5070b9b268
Zi Yan <zi.yan@cs.rutgers.edu>
2015-02-10 16:27:38 -0500

    add training times as cmd parameter

--- src/test.cpp
+++ src/test.cpp
@@ -23,7 +23,7 @@
 #include "neural_layer.h"
 #include "neural_net.h"
 
-int main()
+int main(int argc, char** argv)
 {
     //vector<vector<double> > weight;
     //weight.push_back(vector<double>());
@@ -41,24 +41,37 @@ int main()
     //one_neuron.activate(input, output);
 
     //std::cout<<one_neuron<<std::endl;
+    //
     vector<double> input = {1,0,1,0,1,1,0};
     vector<double> output = {0,1,1,1,0,0,1};
     vector<double> pred;
     vector<double> coef(input.size(),0);
-    base_neural_layer<double, double> *one_neural_layer = new neural_layer<double, double, ACT_FUNC::linear>(input.size(), output.size(), OUTPUT_LAYER_T, 1.0, 0.01);
+    //base_neural_layer<double, double> *one_neural_layer = new neural_layer<double, double, ACT_FUNC::linear>(input.size(), output.size(), OUTPUT_LAYER_T, 1.0, 0.01);
     std::string cmd;
     int count = 0;
 
+    for_each(output.begin(), output.end(), [](double out){std::cout<<out<<" ";});
+    std::cout<<std::endl;
 
-    vector<int> layer_sizes = {52, 60, 52};
-    vector<activate_method_t> methods(2, SIGMOID_T);
+    vector<int> layer_sizes = {7, 10, 10, 7};
+    vector<activate_method_t> methods(3, SIGMOID_T);
+
+    //vector<int> layer_sizes = {7, 7};
+    //vector<activate_method_t> methods(1, LINEAR_T);
 
     neural_net<double, double> one_net(layer_sizes, methods);
+
+    one_net.train(input, output, argc == 2?atoi(argv[1]):10);
+
+    one_net.predict(input, pred);
+
+    for_each(pred.begin(), pred.end(), [](double out){std::cout<<out<<" ";});
+    std::cout<<std::endl;
 
     //std::cout<<"==========="<<std::endl;
     //std::cout<<*(one_neural_layer)<<std::endl;
 
-
+/*
     while(true)
     {
 
@@ -93,6 +106,7 @@ int main()
     std::cout<<count<<std::endl;
 
     delete one_neural_layer;
+    */
 
     //std::cout<<"==========="<<std::endl;
     //std::cout<<one_neural_layer<<std::endl;
